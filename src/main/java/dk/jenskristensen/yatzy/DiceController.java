9e730578fa15181399f5f9071d788d1276c601dd
jenkri5 <jenskristensen@me.com>
2022-03-16 02:41:28 +0100

    color adjust constrast of locked dice

--- src/main/java/dk/jenskristensen/yatzy/DiceController.java
+++ src/main/java/dk/jenskristensen/yatzy/DiceController.java
@@ -6,7 +6,6 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.Random;
 
 public class DiceController {
