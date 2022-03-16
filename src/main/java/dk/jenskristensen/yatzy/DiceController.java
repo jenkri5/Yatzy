d2cdc8084049f41f5919ddcb910d46ca7d595d9e
jenkri5 <jenskristensen@me.com>
2022-03-16 02:33:57 +0100

    color adjust constrast of locked dice

--- src/main/java/dk/jenskristensen/yatzy/DiceController.java
+++ src/main/java/dk/jenskristensen/yatzy/DiceController.java
@@ -1,6 +1,7 @@
 package dk.jenskristensen.yatzy;
 
 import javafx.fxml.FXML;
+import javafx.scene.effect.ColorAdjust;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
@@ -21,27 +22,63 @@ public class DiceController {
     }
 
     public void lockOne() {
+        ColorAdjust colorAdjust = new ColorAdjust();
         DiceThrower.lock(0);
+        if(DiceThrower.m_Dice[0].m_IsLocked)
+            colorAdjust.setContrast(-0.5);
+        else
+            colorAdjust.setContrast(0.0);
+        dice0.setEffect(colorAdjust);
     }
 
     public void lockTwo() {
+        ColorAdjust colorAdjust = new ColorAdjust();
         DiceThrower.lock(1);
+        if(DiceThrower.m_Dice[1].m_IsLocked)
+            colorAdjust.setContrast(-0.5);
+        else
+            colorAdjust.setContrast(0.0);
+        dice1.setEffect(colorAdjust);
     }
 
     public void lockThree() {
+        ColorAdjust colorAdjust = new ColorAdjust();
         DiceThrower.lock(2);
+        if(DiceThrower.m_Dice[2].m_IsLocked)
+            colorAdjust.setContrast(-0.5);
+        else
+            colorAdjust.setContrast(0.0);
+        dice2.setEffect(colorAdjust);
     }
 
     public void lockFour() {
+        ColorAdjust colorAdjust = new ColorAdjust();
         DiceThrower.lock(3);
+        if(DiceThrower.m_Dice[3].m_IsLocked)
+            colorAdjust.setContrast(-0.5);
+        else
+            colorAdjust.setContrast(0.0);
+        dice3.setEffect(colorAdjust);
     }
 
     public void lockFive() {
+        ColorAdjust colorAdjust = new ColorAdjust();
         DiceThrower.lock(4);
+        if(DiceThrower.m_Dice[4].m_IsLocked)
+            colorAdjust.setContrast(-0.5);
+        else
+            colorAdjust.setContrast(0.0);
+        dice4.setEffect(colorAdjust);
     }
 
     public void lockSix() {
+        ColorAdjust colorAdjust = new ColorAdjust();
         DiceThrower.lock(5);
+        if(DiceThrower.m_Dice[5].m_IsLocked)
+            colorAdjust.setContrast(-0.5);
+        else
+            colorAdjust.setContrast(0.0);
+        dice5.setEffect(colorAdjust);
     }
 
     private void updateImage() {
@@ -151,7 +188,7 @@ public class DiceController {
     private static class DiceThrower {
 
         private final static int m_nDice = 6;
-        private static Die[] m_Dice = new Die[m_nDice];
+        private final static Die[] m_Dice = new Die[m_nDice];
         private static int m_RollCount;
 
         private static void generateDice() {
@@ -164,15 +201,9 @@ public class DiceController {
         }
 
         private static void roll() {
-            Arrays.stream(m_Dice).filter(die -> !die.isLocked()).forEach(Die::roll);
+            Arrays.stream(m_Dice).filter(die -> !die.m_IsLocked).forEach(Die::roll);
             m_RollCount++;
             System.out.println(m_RollCount);
-        }
-
-        private static void sort() {
-            m_Dice = Arrays.stream(m_Dice)
-                    .sorted(Comparator.comparing(Die::isLocked).reversed().thenComparingInt(DiceThrower.Die::getFaceValue))
-                    .toArray(Die[]::new);
         }
 
         private static void lock(int i) {
@@ -191,16 +222,8 @@ public class DiceController {
             private int m_FaceValue;
             private boolean m_IsLocked = false;
 
-            private int getFaceValue() {
-                return m_FaceValue;
-            }
-
             private void setLocked() {
                 m_IsLocked = !m_IsLocked;
-            }
-
-            private boolean isLocked() {
-                return m_IsLocked;
             }
 
             private void roll() {
