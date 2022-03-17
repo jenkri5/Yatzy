64bc4d0a0f26493e9e8a81325eae6f4e0a77170f
jenkri5 <jenskristensen@me.com>
2022-03-17 20:47:05 +0100

    added main to launch app

--- src/main/java/dk/jenskristensen/yatzy/App.java
+++ src/main/java/dk/jenskristensen/yatzy/App.java
@@ -20,7 +20,6 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         scene = new Scene(loadFXML("gameScene"));
-        setRoot("gameScene");
         stage.setScene(scene);
         stage.setResizable(false);
         stage.show();
